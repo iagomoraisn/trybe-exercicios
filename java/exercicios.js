1df9f24082c1bed56b3744808a3f8745103ee7b2
iagomoraisn <iagojkd96@hotmail.com>
2023-03-28 19:45:40 -0300

    exercicio js

---
+++ java/exercicios.js
@@ -0,0 +1,31 @@
+/*
+
+Utilize if/else para escrever um código que retorne o maior de dois números. Defina, no começo do seu código, duas variáveis com os valores que serão comparados.
+
+
+let num1 = 10;
+//let num2 = 20;
+
+//if (num1 > num2) {
+    //console.log(num1 + "é maior que " + num2);
+//} else {
+    //console.log(num2 + "é maior que " + num1);
+//}*/
+
+
+
+/*3 - Utilize if/else para escrever um código que retorne o maior de três números. Defina, no começo do seu código, três variáveis com os valores que serão comparados.
+
+
+
+let num1 = 10;
+let num2 = 20;
+let num3 = 15;
+
+if (num1 > num2 && num1 > num3) {
+  console.log(num1 + " é o maior número");
+} else if (num2 > num1 && num2 > num3) {
+  console.log(num2 + " é o maior número");
+} else {
+  console.log(num3 + " é o maior número");
+} */
