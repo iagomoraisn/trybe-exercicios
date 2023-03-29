7a34dd230908a1b75d3191e54eb336b2e08e62ad
iagomoraisn <iagojkd96@hotmail.com>
2023-03-29 13:52:56 -0300

    exercicio xadrez

--- java/exercicios.js
+++ java/exercicios.js
@@ -64,6 +64,33 @@ console.log('erro: angulo invalido');
 
 } */
 
+//6 - Utilize switch/case para escrever um código que receba o nome de uma peça de xadrez e retorne os movimentos que ela pode fazer.
+
+let chessPiece = 'Rei';
+
+switch (chessPiece.toLowerCase()) {
+  case 'rei':
+    console.log('Rei-> Uma casa apenas para qualquer direção.');
+    break;
+  case 'bispo':
+    console.log('Bispo-> Diagonal.');
+    break;
+  case 'rainha':
+    console.log('Rainha-> Diagonal, horizontal e vertical.');
+    break;
+  case 'cavalo':
+    console.log('Cavalo -> "L" pode pular sobre as peças.');
+    break;
+  case 'torre':
+    console.log('Torre -> Horizontal e vertical.');
+    break;
+  case 'peão':
+    console.log("Peão -> Apenas uma casa para frente, no primeiro movimento podem ser duas casas.");
+    break;
+  default:
+    console.log('Erro, peça inválida!');
+};
+
 
 
 
